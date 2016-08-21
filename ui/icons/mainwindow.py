9e7b9a0a02d5edc4df6beaa9a4cab8147856ab23
Hadrian Montes <hadrianmontes@gmail.com>
2016-08-21 19:23:17 +0200

    Changes in the interface

--- ui/icons/mainwindow.py
+++ ui/icons/mainwindow.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-# Form implementation generated from reading ui file 'mainwindow.ui'
+# Form implementation generated from reading ui file 'plotter.ui'
 #
 # Created by: PyQt4 UI code generator 4.11.4
 #
