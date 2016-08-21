4d1bbd360a58f947fd08a250b86b02e4abbe6d17
Hadrian Montes <hadrianmontes@gmail.com>
2016-08-21 21:08:31 +0200

    Improved interface

--- interface.py
+++ interface.py
@@ -70,6 +70,14 @@ class Plotter2(Ui_MainWindow):
         self.semilogy_but.clicked.connect(self.semilogy_scale)
         self.loglog_but.clicked.connect(self.loglog_scale)
 
+        ##########################
+        # Disable items on start #
+        ##########################
+
+        self.itemStatus(False)
+
+        self.bocDiffColor.stateChanged.connect(self.marker_color_policy)
+
     def undo_function(self):
         self.error_dialog_axis()
         print("Hola")
@@ -96,6 +104,7 @@ class Plotter2(Ui_MainWindow):
             if self.selected:
                 self.unremark_axis(self.selected)
             self.selected=None
+        self.itemStatus(bool(self.selected))
         self.canvas.draw()
 
     def remark_axis(self,index):
@@ -187,6 +196,64 @@ class Plotter2(Ui_MainWindow):
                               "Please check plot boundaries and try again")
         error_message.exec_()
 
+    #############################################
+    # Methods to activate and desactivate items #
+    #############################################
+
+    def itemStatus(self,state):
+
+        ##########################
+        # Disable the axe limits #
+        ##########################
+
+        self.xmax_val.setEnabled(state)
+        self.xmin_val.setEnabled(state)
+        self.ymax_val.setEnabled(state)
+        self.ymin_val.setEnabled(state)
+
+        #####################
+        # Disable Axe scale #
+        #####################
+
+        self.decimal_but.setEnabled(state)
+        self.semilogx_but.setEnabled(state)
+        self.semilogy_but.setEnabled(state)
+        self.loglog_but.setEnabled(state)
+
+        #####################
+        # Disable path file #
+        #####################
+
+        self.file_path_box.setEnabled(state)
+        self.file_path_open.setEnabled(state)
+
+        #######################
+        # Disable file column #
+        #######################
+
+        self.xcolumn_index.setEnabled(state)
+        self.y_column_index.setEnabled(state)
+
+        ######################
+        # Disable properties #
+        ######################
+
+        self.comboColor.setEnabled(state)
+        self.comboLinestyle.setEnabled(state)
+        self.comboMarker.setEnabled(state)
+        self.comboMarkerColor.setEnabled(state)
+        self.bocDiffColor.setEnabled(state)
+
+        ####################################
+        # Set the state of the markercolor #
+        ####################################
+        if state:
+            self.marker_color_policy()
+
+    def marker_color_policy(self):
+
+        state=self.bocDiffColor.isChecked()
+        self.comboMarkerColor.setEnabled(state)
 
 if __name__=="__main__":
     import sys
